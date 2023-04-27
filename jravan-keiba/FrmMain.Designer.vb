fa29c1c3df8a48bfb5d710cf4da8d0120fed4ffe
Tomotaka Iwakoshi <tomota8686@gmail.com>
2023-04-27 19:55:41 +0900

    プロジェクト ファイルを追加します。

---
+++ jravan-keiba/FrmMain.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmMain
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmMain))
+        Me.AxJVLink1 = New AxJVDTLabLib.AxJVLink()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.menuConfig = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menuConfJV = New System.Windows.Forms.ToolStripMenuItem()
+        CType(Me.AxJVLink1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'AxJVLink1
+        '
+        Me.AxJVLink1.Enabled = True
+        Me.AxJVLink1.Location = New System.Drawing.Point(89, 74)
+        Me.AxJVLink1.Name = "AxJVLink1"
+        Me.AxJVLink1.OcxState = CType(resources.GetObject("AxJVLink1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxJVLink1.Size = New System.Drawing.Size(192, 192)
+        Me.AxJVLink1.TabIndex = 0
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.menuConfig})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'menuConfig
+        '
+        Me.menuConfig.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.menuConfJV})
+        Me.menuConfig.Name = "menuConfig"
+        Me.menuConfig.Size = New System.Drawing.Size(58, 20)
+        Me.menuConfig.Text = "設定(&C)"
+        '
+        'menuConfJV
+        '
+        Me.menuConfJV.Name = "menuConfJV"
+        Me.menuConfJV.Size = New System.Drawing.Size(180, 22)
+        Me.menuConfJV.Text = "JV-Linkの設定(&J)"
+        '
+        'FrmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.AxJVLink1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "FrmMain"
+        Me.Text = "Form1"
+        CType(Me.AxJVLink1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents AxJVLink1 As AxJVDTLabLib.AxJVLink
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents menuConfig As ToolStripMenuItem
+    Friend WithEvents menuConfJV As ToolStripMenuItem
+End Class
---
+++ jravan-keiba/FrmMain.vb
@@ -0,0 +1,24 @@
+﻿Public Class FrmMain
+    Public Sub New()
+
+        ' この呼び出しはデザイナーで必要です。
+        InitializeComponent()
+
+        ' InitializeComponent() 呼び出しの後で初期化を追加します。
+
+    End Sub
+
+    Private Sub menuConfJV_Click(sender As Object, e As EventArgs) Handles menuConfJV.Click
+        Try
+            'リターンコード
+            Dim lReturnCode As Long
+            '設定画面表示
+            lReturnCode = AxJVLink1.JVSetUIProperties
+            If lReturnCode <> 0 Then
+                MsgBox("JVSetUIPropertiesエラー コード：" & lReturnCode & "：", MessageBoxIcon.Error)
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
